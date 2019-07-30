03e030318de509e4df1df1f9288fd53f50712fb4
zhongyiwei <892151431@qq.com>
2019-07-30 19:35:05 +0800

    BIG DATA WITH SPRINGCLOUD

---
+++ big_data/configuration_center/src/main/java/com/bigdata/configuration/ConfigurationCenterApplication.java
@@ -0,0 +1,13 @@
+package com.bigdata.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationCenterApplication.class, args);
+    }
+
+}
