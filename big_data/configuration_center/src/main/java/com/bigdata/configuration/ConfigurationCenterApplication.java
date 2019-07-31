b52acd4805b85da172f91e2bb7333bbedd1c5e42
zhongyiwei <892151431@qq.com>
2019-07-31 13:46:03 +0800

    添加注册中心

--- big_data/configuration_center/src/main/java/com/bigdata/configuration/ConfigurationCenterApplication.java
+++ big_data/configuration_center/src/main/java/com/bigdata/configuration/ConfigurationCenterApplication.java
@@ -2,8 +2,10 @@ package com.bigdata.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigurationCenterApplication {
 
     public static void main(String[] args) {
